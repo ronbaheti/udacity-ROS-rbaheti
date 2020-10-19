aedf4038a339f92cb103d667f1eaf6d9c536e231
ronbaheti <70448874+ronbaheti@users.noreply.github.com>
2020-10-19 06:22:18 +0530

    Add files via upload

---
+++ Project 5/src/scripts/test_nav.sh
@@ -0,0 +1,8 @@
+xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
+sleep 5
+xterm -e "  roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/world.world" &
+sleep 5
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch " &
+sleep 5
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
